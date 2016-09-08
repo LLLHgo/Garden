cb253805ea600decfbdd9b82cc18ef8590c89ea7
WinwindH <huangpiao@huangpiaodeMacBook-Pro.local>
2016-09-08 20:43:22 +0800

    b

--- LLLH/src/teamTest/teamTest.java
+++ LLLH/src/teamTest/teamTest.java
@@ -3,7 +3,8 @@ package teamTest;
 public class teamTest {
 
 	public static void main(String[] args) {
-		System.out.println("we are a team!");
+		System.out.println("we are a team! ");
+		System.out.println("^_^");
 	}
 
 }
