abf023c8312ebe0b605055257bc59e7c791a3d24
WinwindH <huangpiao@huangpiaodeMacBook-Pro.local>
2016-09-08 20:54:26 +0800

    hahahahaha

--- LLLH/src/teamTest/teamTest.java
+++ LLLH/src/teamTest/teamTest.java
@@ -5,6 +5,7 @@ public class teamTest {
 	public static void main(String[] args) {
 		System.out.println("we are a team!");
 		System.out.println("LLLHgo!");
+		System.out.println("I am coming back.^_^");
 	}
 
 }
